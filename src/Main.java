7315c7cc926b6fbcf2441e1f292d3ce5364b43fa
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-05-30 19:28:52 +0300

    Add linear search algorithm.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 9, 11, 13};
+        int target = 11;
+
+        int result = Search.linearSearch(nums, target);
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
