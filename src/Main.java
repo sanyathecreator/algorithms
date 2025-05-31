abe3712e7607a4c5c0a85c5aef3b062f5f3bf909
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-05-31 23:48:02 +0200

    Add bubble sort algorithm.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {5, 7, 9, 11, 13};
-        int target = 13;
-
-        int result = Search.binarySearchRecursive(nums, target, 0, nums.length - 1);
-        if (result != -1)
-            System.out.println("Element found at index: " + result);
-        else
-            System.out.println("Element not found");
+        int[] nums = {11, 9, 7, 13, 5};
+        int[] sorted = Sort.bubbleSort(nums);
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.println(sorted[i]);
+        }
     }
 }
---
+++ src/Sort.java
@@ -0,0 +1,14 @@
+public class Sort {
+    public static int[] bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
