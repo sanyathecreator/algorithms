55c99f039cd1a158bf51e6219889cb7c5abf2284
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-06-01 23:08:34 +0200

    Add selection sort algorithm.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {11, 9, 7, 13, 5};
-        int[] sorted = Sort.bubbleSort(nums);
+        int[] nums = {0, 1, 11, 9, 7, 13, 5, 22, -2, 13, 8, 6, 2, 3};
+        int[] sorted = Sort.selectionSort(nums);
         for (int i = 0; i < sorted.length; i++) {
             System.out.println(sorted[i]);
         }
